60be7f2192e9f85b9c74b55108d7a51ceede417f
ThanhNghi <nghilt19411@gmail.com>
2022-02-10 14:36:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
+const courses = [
+  {
+    id:1,
+    name:'HTML, CSS'
+  },
+  {
+    id:2,
+    name:'Javascript'
+  },
+  {
+    id:3,
+    name:'ReactJs'
+  },
+
+]
 function App() {
+                     {/* Radio: */}
+  // const [checked , setChecked] = useState();
+  // const handleSubmit =()=>{
+  //   console.log({
+  //     id:checked
+  //   })
+  // }
+                        {/* Checkbox: */}
+    const [checked , setChecked] = useState([]);
+    const handleCheck = (id) =>{
+      setChecked(prev => {
+        const isChecked = checked.includes(id);
+        if(isChecked){
+          return checked.filter(item => item !==id)
+        }else{
+          return [...prev,id]
+        }
+      
+      })
+     
+     
+    }
+    const handleSubmit = () =>{
+      console.log({
+        ids:checked
+      })
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+                        {/* Radio: */}
+    {/* {courses.map(course => (
+      <div key={course.id}>
+        <input 
+          type="radio"
+          onChange={()=>setChecked(course.id) }
+          checked={checked===course.id}
+          /> 
+        {course.name}
+      </div>
+    ))} */}
+                          {/* Checkbox: */}
+    {courses.map(course => (
+      <div key={course.id}>
+        <input 
+          type="checkbox"
+          onChange={()=> handleCheck(course.id) }
+          checked={checked.includes(course.id)}
+          /> 
+        {course.name}
+      </div>
+    ))}
+    <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
